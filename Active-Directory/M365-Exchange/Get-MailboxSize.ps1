45628e59bcd812977490975fd7d86d3b60cbd46d
Tode Despotoski <tode.despotoski@gmail.com>
2026-05-15 20:28:21 +0100

    Delete Active-Directory/M365-Exchange/Get-MailboxSize.ps1

--- Active-Directory/M365-Exchange/Get-MailboxSize.ps1
+++
@@ -1,6 +0,0 @@
-<#
-.SYNOPSIS
-Retrieves the total mailbox size for a specific user in Exchange Online.
-#>
-
-Get-ExoMailboxStatistics -Identity "user@domain.com" | Select-Object DisplayName, TotalItemSize
